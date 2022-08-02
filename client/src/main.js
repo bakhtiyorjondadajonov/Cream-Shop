515b37a2f273b3f459468531da387c0df579ccfc
bakhtiyorjondadajonov <bakhtiyorjondadajonov@gmail.com>
2022-08-02 15:38:05 +0900

    Section brand is finished

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,17 @@
 const elem = document.querySelector(".main-carousel");
+const brandCorusel = document.querySelector(".brand-carousel");
 let flkty = new Flickity(elem, {
-  // autoPlay: true,
+  autoPlay: true,
   cellAlign: "left",
   contain: true,
+  freeScroll: true,
+  prevNextButtons: false,
+  pageDots: false,
+});
+
+let secondCorusel = new Flickity(brandCorusel, {
+  cellAlign: "left",
+  contain: true,
+  freeScroll: true,
+  prevNextButtons: false,
 });
